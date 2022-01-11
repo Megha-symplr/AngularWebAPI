e3cf32b3a63edded53d41aad99e2ba9d6a7fbcae
Megha-symplr <megham@symplr.com>
2022-01-11 22:49:05 +0530

    Added fronend part

---
+++ src/app/department/show-dep/show-dep.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit ,Input} from '@angular/core';
+import { SharedService } from 'src/app/shared.service';
+
+
+@Component({
+  selector: 'app-show-dep',
+  templateUrl: './show-dep.component.html',
+  styleUrls: ['./show-dep.component.css']
+})
+export class ShowDepComponent implements OnInit {
+
+  constructor(private service:SharedService) { }
+
+  DepartmentList:any=[];
+
+  ModalTitle!: string;
+  ActivateAddEditDepComp:boolean=false;
+ @Input() dep:any;
+
+  ngOnInit(): void {
+    this.refreshDepList();
+  }
+
+  addClick(){
+    this.dep={
+      DepartmentId:0,
+      DepartmentName:""
+    }
+    this.ModalTitle="Add Department";
+    this.ActivateAddEditDepComp=true;
+
+  }
+
+  editClick(item:any){
+      this.dep=item;
+      this.ModalTitle="Edit Department";
+      this.ActivateAddEditDepComp=true;
+  }
+  deleteClick(item:any){
+     if(confirm('Are you sure??')){
+       this.service.deleteDepartment(item.DepartmentId).subscribe(data=>{
+         alert(data.toString());
+         this.refreshDepList();
+       })
+     }
+  }
+  closeClick(){
+    this.ActivateAddEditDepComp =false;
+    this.refreshDepList();
+  }
+
+  refreshDepList(){
+    this.service.getDepList().subscribe(data=>{
+     this.DepartmentList=data;
+     });
+   
+  }
+  
+
+}
