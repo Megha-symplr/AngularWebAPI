60125b8dee27a3b405a89be3d6327a2e50dc51c6
Megha-symplr <megham@symplr.com>
2022-01-12 10:30:08 +0530

    added backend

--- WebApplication1/Models/Employee.cs
+++ WebApplication1/Models/Employee.cs
@@ -10,8 +10,8 @@ namespace WebApplication1.Models
         public int EmployeeId { get; set; }
 
         public string EmployeeName { get; set; }
-        public string Department { get; set; }
 
+        public string Department { get; set; }
         public string DateOfJoining { get; set; }
     }
 }
